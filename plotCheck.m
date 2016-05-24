0b52e4e017737172e4eeaf29a1f87c9aa6a26d99
Wei-Cheng Yang <rainyday309@gmail.com>
2016-05-24 08:07:42 +0800

    add: specify plotCheck channel parameter for batch plotting

--- plotCheck.m
+++ plotCheck.m
@@ -4,19 +4,21 @@
 % 2nd column contain an cell array 1x3
 % each cell array contain an array 1280x20
 
-function plotCheck(name, data)
+function plotCheck(name, data, channel)
 [length,~] = size(data);
 
 
 
 for i=1:length
     subplot(length,1,i);
-    plot(data{i}(:,1));
+    plot(data{i}(:,channel));
 end
 
 subplot(length,1,1);
 title(name);
 
 
+
+saveas(gcf,[pwd strcat('/recheck_plots/',name,'_channel_',num2str(channel),'.jpg')]);
 
 end
