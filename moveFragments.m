f56bb206ad2530ab23f55de0826a71946a83b74d
Wei-Cheng Yang <rainyday309@gmail.com>
2016-05-31 13:56:29 +0800

    fix moveFragments bug for not able to modify global variable

--- moveFragments.m
+++ moveFragments.m
@@ -1,11 +1,17 @@
 % moveFragments.m
 % move selected epochs to selected epochs 
 
-function moveFragments(input,line)
+function moveFragments(line)
 
-for i=1:3
-    input{line,2}{i} = input{line,3}{input{line,4}(i)};
-    plotChannels(input{line,2}{i});
+for loop=1:3
+    order = strrep('selected{line,2}{loop}=selected{line,3}{selected{line,4}(loop)};','line',num2str(line));
+    order = strrep(order,'loop',num2str(loop));
+    plotorder = strrep('plotChannels(selected{line,2}{loop});','line',num2str(line));
+    plotorder = strrep(plotorder,'loop',num2str(loop));
+    % disp(order);
+    % disp(plotorder);
+    evalin('base',order);
+    evalin('base',plotorder);
 end
 
 end
