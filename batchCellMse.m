04de60c412635d1bb71068168196cb56ccbe4305
Wei-Cheng Yang <rainyday309@gmail.com>
2016-06-07 07:32:27 +0800

    batchCellMse.m: for calculating mse of data in cell array

---
+++ batchCellMse.m
@@ -0,0 +1,10 @@
+% batchCellMse.m
+% calculate mse for data in cell array form
+% input first column: name of data
+% input second column: cell array containing 3 epoch data
+
+function output = batchCellMse(input)
+
+
+
+end
