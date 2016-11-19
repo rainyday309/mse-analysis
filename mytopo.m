ef7a2db57069c287b9fed4d8862a7a6fdd04f1ce
Wei-Cheng Yang <rainyday309@gmail.com>
2016-11-19 12:59:57 +0800

    mytopo modification

--- mytopo.m
+++ mytopo.m
@@ -2,6 +2,6 @@
 % draw topoplot with imported vector
 function mytopo(input, limits)
   places = readlocs('10-20.ced');
-  topoplot(input,places,'headrad',0.51,'maplimits',limits);
+  topoplot(input,places,'headrad',0.53,'maplimits',limits);
   
 end
